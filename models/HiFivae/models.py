6e615e1b37c462a0dcafedb68b1c4ab95ef25511
autumn <109412646+autumn-2-net@users.noreply.github.com>
2023-11-24 00:11:01 +0800

    add HIFIvae

--- models/HiFivae/models.py
+++ models/HiFivae/models.py
@@ -16,28 +16,30 @@ class Encoder(nn.Module):
         self.h = h
         # h["inter_channels"]
         self.num_kernels = len(h["resblock_kernel_sizes"])
-        self.out_channels = h["inter_channels"]
+        self.out_channels = h["num_mels"]
         self.num_downsamples = len(h["upsample_rates"])
-        self.conv_pre = weight_norm(Conv1d(1, h["upsample_initial_channel"]// (2 ** len(h["upsample_rates"])), 7, 1, padding=3))
+        self.conv_pre = weight_norm(
+            Conv1d(1, h["upsample_initial_channel"] // (2 ** len(h["upsample_rates"])), 7, 1, padding=3))
         resblock = ResBlock1 if h["resblock"] == '1' else ResBlock2
         self.ups = nn.ModuleList()
         for i, (u, k) in enumerate(zip(reversed(h["upsample_rates"]), reversed(h["upsample_kernel_sizes"]))):
             self.ups.append(weight_norm(
-                Conv1d(h["upsample_initial_channel"] // (2 ** (len(h["upsample_rates"]) - i)), h["upsample_initial_channel"] // (2 ** (len(h["upsample_rates"]) - i - 1)),
-                                k, u, padding=(k - u +1 ) // 2)))
+                Conv1d(h["upsample_initial_channel"] // (2 ** (len(h["upsample_rates"]) - i)),
+                       h["upsample_initial_channel"] // (2 ** (len(h["upsample_rates"]) - i - 1)),
+                       k, u, padding=(k - u + 1) // 2)))
         self.resblocks = nn.ModuleList()
         for i in range(len(self.ups), 0, -1):
-            ch = h["upsample_initial_channel"] // (2 ** (i-1))
+            ch = h["upsample_initial_channel"] // (2 ** (i - 1))
             for j, (k, d) in enumerate(zip(h["resblock_kernel_sizes"], h["resblock_dilation_sizes"])):
                 self.resblocks.append(resblock(h, ch, k, d))
 
-        self.conv_post = weight_norm(Conv1d(ch, 2 * h["inter_channels"], 7, 1, padding=3))
+        self.conv_post = weight_norm(Conv1d(ch, 2 * h["num_mels"], 7, 1, padding=3))
         self.ups.apply(init_weights)
         self.conv_post.apply(init_weights)
         self.upp = np.prod(h["upsample_rates"])
 
     def forward(self, x):
-        x = x[:,None,:]
+        # x = x[:, None, :]
         x = self.conv_pre(x)
         for i in range(self.num_downsamples):
             x = F.leaky_relu(x, LRELU_SLOPE)
@@ -54,6 +56,8 @@ class Encoder(nn.Module):
         m, logs = torch.split(x, self.out_channels, dim=1)
         z = m + torch.randn_like(m) * torch.exp(logs)
         return z, m, logs
+
+
 class AttrDict(dict):
     def __init__(self, *args, **kwargs):
         super(AttrDict, self).__init__(*args, **kwargs)
@@ -68,6 +72,7 @@ def init_weights(m, mean=0.0, std=0.01):
 
 def get_padding(kernel_size, dilation=1):
     return int((kernel_size * dilation - dilation) / 2)
+
 
 class ResBlock1(torch.nn.Module):
     def __init__(self, h, channels, kernel_size=3, dilation=(1, 3, 5)):
@@ -133,160 +138,32 @@ class ResBlock2(torch.nn.Module):
             remove_weight_norm(l)
 
 
-class SineGen(torch.nn.Module):
-    """ Definition of sine generator
-    SineGen(samp_rate, harmonic_num = 0,
-            sine_amp = 0.1, noise_std = 0.003,
-            voiced_threshold = 0,
-            flag_for_pulse=False)
-    samp_rate: sampling rate in Hz
-    harmonic_num: number of harmonic overtones (default 0)
-    sine_amp: amplitude of sine-waveform (default 0.1)
-    noise_std: std of Gaussian noise (default 0.003)
-    voiced_threshold: F0 threshold for U/V classification (default 0)
-    flag_for_pulse: this SinGen is used inside PulseGen (default False)
-    Note: when flag_for_pulse is True, the first time step of a voiced
-        segment is always sin(np.pi) or cos(0)
-    """
-
-    def __init__(self, samp_rate, harmonic_num=0,
-                 sine_amp=0.1, noise_std=0.003,
-                 voiced_threshold=0):
-        super(SineGen, self).__init__()
-        self.sine_amp = sine_amp
-        self.noise_std = noise_std
-        self.harmonic_num = harmonic_num
-        self.dim = self.harmonic_num + 1
-        self.sampling_rate = samp_rate
-        self.voiced_threshold = voiced_threshold
-
-    def _f02uv(self, f0):
-        # generate uv signal
-        uv = torch.ones_like(f0)
-        uv = uv * (f0 > self.voiced_threshold)
-        return uv
-
-    def _f02sine(self, f0_values, upp):
-        """ f0_values: (batchsize, length, dim)
-            where dim indicates fundamental tone and overtones
-        """
-        rad_values = (f0_values / self.sampling_rate).fmod(1.)  # %1意味着n_har的乘积无法后处理优化
-        rand_ini = torch.rand(1, self.dim, device=f0_values.device)
-        rand_ini[:, 0] = 0
-        rad_values[:, 0, :] += rand_ini
-        is_half = rad_values.dtype is not torch.float32
-        tmp_over_one = torch.cumsum(rad_values.double(), 1)  # % 1  #####%1意味着后面的cumsum无法再优化
-        if is_half:
-            tmp_over_one = tmp_over_one.half()
-        else:
-            tmp_over_one = tmp_over_one.float()
-        tmp_over_one *= upp
-        tmp_over_one = F.interpolate(
-            tmp_over_one.transpose(2, 1), scale_factor=upp,
-            mode='linear', align_corners=True
-        ).transpose(2, 1)
-        rad_values = F.interpolate(rad_values.transpose(2, 1), scale_factor=upp, mode='nearest').transpose(2, 1)
-        tmp_over_one = tmp_over_one.fmod(1.)
-        diff = F.conv2d(
-            tmp_over_one.unsqueeze(1), torch.FloatTensor([[[[-1.], [1.]]]]).to(tmp_over_one.device),
-            stride=(1, 1), padding=0, dilation=(1, 1)
-        ).squeeze(1)  # Equivalent to torch.diff, but able to export ONNX
-        cumsum_shift = (diff < 0).double()
-        cumsum_shift = torch.cat((
-            torch.zeros((f0_values.size()[0], 1, self.dim), dtype=torch.double).to(f0_values.device),
-            cumsum_shift
-        ), dim=1)
-        sines = torch.sin(torch.cumsum(rad_values.double() + cumsum_shift, dim=1) * 2 * np.pi)
-        if is_half:
-            sines = sines.half()
-        else:
-            sines = sines.float()
-        return sines
-
-    @torch.no_grad()
-    def forward(self, f0, upp):
-        """ sine_tensor, uv = forward(f0)
-        input F0: tensor(batchsize=1, length, dim=1)
-                  f0 for unvoiced steps should be 0
-        output sine_tensor: tensor(batchsize=1, length, dim)
-        output uv: tensor(batchsize=1, length, 1)
-        """
-        f0 = f0.unsqueeze(-1)
-        fn = torch.multiply(f0, torch.arange(1, self.dim + 1, device=f0.device).reshape((1, 1, -1)))
-        sine_waves = self._f02sine(fn, upp) * self.sine_amp
-        uv = (f0 > self.voiced_threshold).float()
-        uv = F.interpolate(uv.transpose(2, 1), scale_factor=upp, mode='nearest').transpose(2, 1)
-        noise_amp = uv * self.noise_std + (1 - uv) * self.sine_amp / 3
-        noise = noise_amp * torch.randn_like(sine_waves)
-        sine_waves = sine_waves * uv + noise
-        return sine_waves
-
-
-class SourceModuleHnNSF(torch.nn.Module):
-    """ SourceModule for hn-nsf
-    SourceModule(sampling_rate, harmonic_num=0, sine_amp=0.1,
-                 add_noise_std=0.003, voiced_threshod=0)
-    sampling_rate: sampling_rate in Hz
-    harmonic_num: number of harmonic above F0 (default: 0)
-    sine_amp: amplitude of sine source signal (default: 0.1)
-    add_noise_std: std of additive Gaussian noise (default: 0.003)
-        note that amplitude of noise in unvoiced is decided
-        by sine_amp
-    voiced_threshold: threhold to set U/V given F0 (default: 0)
-    Sine_source, noise_source = SourceModuleHnNSF(F0_sampled)
-    F0_sampled (batchsize, length, 1)
-    Sine_source (batchsize, length, 1)
-    noise_source (batchsize, length 1)
-    uv (batchsize, length, 1)
-    """
-
-    def __init__(self, sampling_rate, harmonic_num=0, sine_amp=0.1,
-                 add_noise_std=0.003, voiced_threshold=0):
-        super(SourceModuleHnNSF, self).__init__()
-
-        self.sine_amp = sine_amp
-        self.noise_std = add_noise_std
-
-        # to produce sine waveforms
-        self.l_sin_gen = SineGen(sampling_rate, harmonic_num,
-                                 sine_amp, add_noise_std, voiced_threshold)
-
-        # to merge source harmonics into a single excitation
-        self.l_linear = torch.nn.Linear(harmonic_num + 1, 1)
-        self.l_tanh = torch.nn.Tanh()
-
-    def forward(self, x, upp):
-        sine_wavs = self.l_sin_gen(x, upp)
-        sine_merge = self.l_tanh(self.l_linear(sine_wavs))
-        return sine_merge
-
-
 class Generator(torch.nn.Module):
     def __init__(self, h):
         super(Generator, self).__init__()
         self.h = h
         self.num_kernels = len(h.resblock_kernel_sizes)
         self.num_upsamples = len(h.upsample_rates)
-        self.m_source = SourceModuleHnNSF(
-            sampling_rate=h.sampling_rate,
-            harmonic_num=8
-        )
-        self.noise_convs = nn.ModuleList()
+        # self.m_source = SourceModuleHnNSF(
+        #     sampling_rate=h.sampling_rate,
+        #     harmonic_num=8
+        # )
+        # self.noise_convs = nn.ModuleList()
         self.conv_pre = weight_norm(Conv1d(h.num_mels, h.upsample_initial_channel, 7, 1, padding=3))
         resblock = ResBlock1 if h.resblock == '1' else ResBlock2
 
         self.ups = nn.ModuleList()
         for i, (u, k) in enumerate(zip(h.upsample_rates, h.upsample_kernel_sizes)):
-            c_cur = h.upsample_initial_channel // (2 ** (i + 1))
+            # c_cur = h.upsample_initial_channel // (2 ** (i + 1))
             self.ups.append(weight_norm(
                 ConvTranspose1d(h.upsample_initial_channel // (2 ** i), h.upsample_initial_channel // (2 ** (i + 1)),
                                 k, u, padding=(k - u) // 2)))
-            if i + 1 < len(h.upsample_rates):  #
-                stride_f0 = int(np.prod(h.upsample_rates[i + 1:]))
-                self.noise_convs.append(Conv1d(
-                    1, c_cur, kernel_size=stride_f0 * 2, stride=stride_f0, padding=stride_f0 // 2))
-            else:
-                self.noise_convs.append(Conv1d(1, c_cur, kernel_size=1))
+            # if i + 1 < len(h.upsample_rates):  #
+            #     stride_f0 = int(np.prod(h.upsample_rates[i + 1:]))
+            #     self.noise_convs.append(Conv1d(
+            #         1, c_cur, kernel_size=stride_f0 * 2, stride=stride_f0, padding=stride_f0 // 2))
+            # else:
+            #     self.noise_convs.append(Conv1d(1, c_cur, kernel_size=1))
         self.resblocks = nn.ModuleList()
         ch = h.upsample_initial_channel
         for i in range(len(self.ups)):
@@ -299,14 +176,14 @@ class Generator(torch.nn.Module):
         self.conv_post.apply(init_weights)
         self.upp = int(np.prod(h.upsample_rates))
 
-    def forward(self, x, f0):
-        har_source = self.m_source(f0, self.upp).transpose(1, 2)
+    def forward(self, x):
+        # har_source = self.m_source(f0, self.upp).transpose(1, 2)
         x = self.conv_pre(x)
         for i in range(self.num_upsamples):
             x = F.leaky_relu(x, LRELU_SLOPE)
             x = self.ups[i](x)
-            x_source = self.noise_convs[i](har_source)
-            x = x + x_source
+            # x_source = self.noise_convs[i](har_source)
+            # x = x + x_source
             xs = None
             for j in range(self.num_kernels):
                 if xs is None:
@@ -329,6 +206,18 @@ class Generator(torch.nn.Module):
             l.remove_weight_norm()
         remove_weight_norm(self.conv_pre)
         remove_weight_norm(self.conv_post)
+
+
+class HiFivae(torch.nn.Module):
+    def __init__(self, h):
+        super().__init__()
+        self.Generator = Generator(h)
+        self.Encoder = Encoder(h)
+
+    def forward(self, x):
+        z, m, logs = self.Encoder(x)
+        x = self.Generator(z)
+        return x
 
 
 class DiscriminatorP(torch.nn.Module):
@@ -418,13 +307,11 @@ class MultiPeriodDiscriminator(torch.nn.Module):
 
         fmap_rs = []
 
-
         for i, d in enumerate(self.discriminators):
             y_d_r, fmap_r = d(y)
 
             y_d_rs.append(y_d_r)
             fmap_rs.append(fmap_r)
-
 
         return y_d_rs, fmap_rs,
 
@@ -490,7 +377,6 @@ class MultiScaleDiscriminator(torch.nn.Module):
             y_d_rs.append(y_d_r)
             fmap_rs.append(fmap_r)
 
-
         return y_d_rs, fmap_rs,
 
 
@@ -510,7 +396,7 @@ def discriminator_loss(disc_real_outputs, disc_generated_outputs):
 
     for dr, dg in zip(disc_real_outputs, disc_generated_outputs):
         r_loss = torch.mean((1 - dr) ** 2)
-        g_loss = torch.mean(dg**2)
+        g_loss = torch.mean(dg ** 2)
         loss += r_loss + g_loss
         r_losses.append(r_loss.item())
         g_losses.append(g_loss.item())
