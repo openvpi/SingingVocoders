6c093a0a82dc5f3d40fc049dbb8b896a72946268
yqzhishen <yangqian_1015@icloud.com>
2023-11-29 02:07:17 +0800

    Support selecting specific ckpt to export

--- export_ckpt.py
+++ export_ckpt.py
@@ -1,4 +1,3 @@
-import importlib
 import pathlib
 
 import click
@@ -6,26 +5,29 @@ import torch
 from tqdm import tqdm
 
 from utils import get_latest_checkpoint_path
-from utils.config_utils import read_full_config
 
 
 @click.command(help='')
-@click.option('--exp_name', required=True, metavar='EXP', help='Name of the experiment')
-@click.option('--save_path', required=True, metavar='EXP', help='Name of the experiment')
-@click.option('--work_dir', required=False, metavar='DIR', help='Directory to save the experiment')
-def train(exp_name, save_path, work_dir):
+@click.option('--exp_name', required=False, metavar='EXP', help='Name of the experiment')
+@click.option('--ckpt_path', required=False, metavar='FILE', help='Path to the checkpoint file')
+@click.option('--save_path', required=True, metavar='FILE', help='Path to save the exported checkpoint')
+@click.option('--work_dir', required=False, metavar='DIR', help='Working directory containing the experiments')
+def export(exp_name, ckpt_path, save_path, work_dir):
     # print_config(config)
-    if work_dir is None:
-        work_dir = pathlib.Path(__file__).parent / 'experiments'
-    else:
-        work_dir = pathlib.Path(work_dir)
-    work_dir = work_dir / exp_name
-    assert not work_dir.exists() or work_dir.is_dir(), f'Path \'{work_dir}\' is not a directory.'
-    work_dir.mkdir(parents=True, exist_ok=True)
+    if not exp_name is None and ckpt_path is None:
+        raise RuntimeError('Either --exp_name or --ckpt_path should be specified.')
+    if ckpt_path is None:
+        if work_dir is None:
+            work_dir = pathlib.Path(__file__).parent / 'experiments'
+        else:
+            work_dir = pathlib.Path(work_dir)
+        work_dir = work_dir / exp_name
+        assert not work_dir.exists() or work_dir.is_dir(), f'Path \'{work_dir}\' is not a directory.'
+        ckpt_path = get_latest_checkpoint_path(work_dir)
 
     ckp = {}
 
-    aaa2x = torch.load(get_latest_checkpoint_path(work_dir))['state_dict']
+    aaa2x = torch.load(ckpt_path)['state_dict']
     for i in tqdm(aaa2x):
         i: str
         if 'generator.' in i:
@@ -36,4 +38,4 @@ def train(exp_name, save_path, work_dir):
 
 
 if __name__ == '__main__':
-    train()
+    export()
